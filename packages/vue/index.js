561881b7e0e5cb9797751b9b00051b8118b3902d
zaeyee <zaeyee@163.com>
2023-01-04 15:03:07 +0800

    fix: prettier config sort

--- packages/vue/index.js
+++ packages/vue/index.js
@@ -1,9 +1,9 @@
 module.exports = {
   extends: [
+    // plugin-vue的推荐配置
+    'plugin:vue/vue3-recommended',
     // @zaeyee的基础配置
     '@zaeyee/eslint-config-base',
-    // plugin-vue的推荐配置
-    'plugin:vue/vue3-recommended'
   ],
   rules: {
     // 关闭vue组件名称必须多单词的限制
